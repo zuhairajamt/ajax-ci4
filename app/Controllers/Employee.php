b39402ca241d76de963c227c4984214f55fd8464
gerrypf <73151831+gerrypf@users.noreply.github.com>
2022-08-03 14:44:26 +0700

    update controller employee tambah id desa

--- app/Controllers/Employee.php
+++ app/Controllers/Employee.php
@@ -2,9 +2,9 @@
 
 namespace App\Controllers;
 
+use CodeIgniter\Controller;
 use App\Models\Employee_model;
 use App\Models\WilayahModel;
-use CodeIgniter\Controller;
 
 class Employee extends Controller
 {
@@ -17,12 +17,15 @@ class Employee extends Controller
     public function index()
     {
         $session = session();
-        
         $uname['user_name'] = $session->get('user_name');
         $uname['role'] = $session->get('role');
 
         $model = new WilayahModel();
         $wil['prov'] = $model->getprovinsi();
+
+        // $model = new Employee_model;
+        // $data['title']     = 'Data Vaksin Karyawan';
+        // $data['getKaryawan'] = $model->getKaryawan();
 
         echo view('header', $uname);
         echo view('employee_view', $wil);
@@ -41,46 +44,52 @@ class Employee extends Controller
                 'rules' => 'required|max_length[50]',
                 'errors' => [
                     'required' => 'Nama Karyawan is required',
-                    'max_length' => 'Your name is too long',
-                ],
+                    'max_length' => 'Your name is too long'
+                ]
             ],
             'usia' => [
                 'rules' => 'required|integer|greater_than_equal_to[10]|less_than_equal_to[100]',
                 'errors' => [
                     'required' => 'Usia is required',
-                ],
+                ]
             ],
             'status_vaksin_1' => [
                 'rules' => 'required',
                 'errors' => [
-                    'required' => 'Status Vaksin 1 is required',
-                ],
+                    'required' => 'Status Vaksin 1 is required'
+                ]
             ],
             'status_vaksin_2' => [
                 'rules' => 'required',
                 'errors' => [
-                    'required' => 'Status Vaksin 2 is required',
-                ],
-            ],
+                    'required' => 'Status Vaksin 2 is required'
+                ]
+            ]
         ]);
 
-        if ($validation->run() == false) {
+        if ($validation->run() == FALSE) {
             $errors = $validation->getErrors();
             echo json_encode(['code' => 0, 'error' => $errors]);
         } else {
             $session = session();
+            //$user_id['user_id'] = $session->get('user_id');
             $user_id = $_SESSION['user_id'];
+            //$user_id = $_SESSION['user_id'];
+            //$autoload['libraries'] = array('session');
+
+            //Insert data into db
 
             $array = [
                 'user_id'   => $user_id,
             ];
 
+            //Insert data into db
             $data = [
-                'nama_karyawan' => $this->request->getPost('nama_karyawan'),
-                'usia' => $this->request->getPost('usia'),
-                'status_vaksin_1' => $this->request->getPost('status_vaksin_1'),
-                'status_vaksin_2' => $this->request->getPost('status_vaksin_2'),
-                'desa'            => $this->request->getPost('desa')
+                'nama_karyawan'     => $this->request->getPost('nama_karyawan'),
+                'usia'              => $this->request->getPost('usia'),
+                'status_vaksin_1'   => $this->request->getPost('status_vaksin_1'),
+                'status_vaksin_2'   => $this->request->getPost('status_vaksin_2'),
+                'desa'              => $this->request->getPost('desa')
             ];
             $builder->set($array);
             $builder->set($data);
@@ -134,11 +143,21 @@ class Employee extends Controller
         
         // var_dump($data);
         echo json_encode($data);
-    }    
+    }
 
     public function getAllEmployee()
     {
-        //DB Details
+        // $db = \Config\Database::connect();
+        // $builder = $db->table('employees')
+        //         ->select(['nama_karyawan', 'usia', 'status_vaksin_1', 'status_vaksin_2', 'deleted_at'])
+        //         ->where('deleted_at',NULL);
+        // // $query = $builder->where('deleted_at',NULL);
+
+        // return DataTable::of($builder)
+        //         ->addNumbering()
+        //         ->toJson();
+
+        // DB Details
         $dbDetails = array(
             "host" => $this->db->hostname,
             "user" => $this->db->username,
@@ -149,28 +168,25 @@ class Employee extends Controller
         $table = <<<EOT
         (
             SELECT
-                employees.id,
-                employees.nama_karyawan,
-                employees.usia,
-                employees.status_vaksin_1,
-                employees.status_vaksin_2,
-                villages.desa,
-                districts.kec,
-                regencies.kota,
-                provinces.prov,
-                employees.deleted_at,
-                employees.user_id
+            employees.id,
+            employees.nama_karyawan,
+            employees.usia,
+            employees.status_vaksin_1,
+            employees.status_vaksin_2,
+            employees.alamat,
+            villages.desa,
+            districts.kec,
+            regencies.kota,
+            provinces.prov,
+            employees.deleted_at
             FROM employees
-            LEFT JOIN villages ON villages.id_desa = employees.desa 
+            LEFT JOIN villages ON villages.id_desa = employees.alamat 
             LEFT JOIN districts ON districts.id_kec = villages.district_id
             LEFT JOIN regencies ON regencies.id_kota = districts.regency_id
             LEFT JOIN provinces ON provinces.id_prov = regencies.province_id
         ) temp
         EOT;
-
         $primaryKey = "id";
-
-        //$query = 'SELECT * FROM employee WHERE user_id = 1';
 
         $columns = array(
             array(
@@ -210,25 +226,29 @@ class Employee extends Controller
                 "dt" => 8,
             ),
             array(
-                "db" => "id",
+                "db" => "alamat",
                 "dt" => 9,
+            ),
+            array(
+                "db" => "id",
+                "dt" => 10,
                 "formatter" => function ($d, $row) {
                     return "<div class='btn-group'>
                                   <a class='btn btn-success btn-edit' data-id='" . $row['id'] . "' data-bs-toggle='modal' data-bs-target='#editModal' id='updateEmployeeBtn' style='margin-right: 10px'><i class='ti ti-edit'></i></a>
                                   <button class='btn btn btn-danger' data-id='" . $row['id'] . "' id='deleteEmployeeBtn'> <i class='ti ti-trash'></i></button>
                              </div>";
-                },
+                }
             ),
         );
 
         $session = session();
-
+        //$user_id['user_id'] = $session->get('user_id');
         $user_id = $_SESSION['user_id'];
-        $user_role =$_SESSION['role'];
+        $role = $_SESSION['role'];
 
-        if($user_role === 'Admin') {
+        if($role === 'admin') {
             echo json_encode(
-                \SSP::simple( $_GET, $dbDetails, $table, $primaryKey, $columns, null, "deleted_at IS NULL" )
+                \SSP::simple($_GET, $dbDetails, $table, $primaryKey, $columns, null, "deleted_at IS NULL")
             );
         } else {
             echo json_encode(
@@ -263,14 +283,6 @@ class Employee extends Controller
         }
     }
 
-    // public function edit()
-    // {
-    //     $model = new Employee_model();
-    //     $id = $this->request->getPost("edit_id");
-    //     $data['employee'] = $model->find($id);
-    //     return $this->response->setJSON($data);
-    // }
-
     public function edit()
     {
         $model = new Employee_model();
@@ -292,49 +304,49 @@ class Employee extends Controller
             'nama_karyawan' => [
                 'rules' => 'required|max_length[50]',
                 'errors' => [
-                    'required' => 'Nama Karyawan is required',
-                ],
+                    'required' => 'Nama Karyawan is required'
+                ]
             ],
             'usia' => [
                 'rules' => 'required|integer|greater_than_equal_to[10]|less_than_equal_to[100]',
                 'errors' => [
-                    'required' => 'Usia is required',
-                ],
+                    'required' => 'Usia is required'
+                ]
             ],
             'status_vaksin_1' => [
                 'rules' => 'required',
                 'errors' => [
-                    'required' => 'Status Vaksin 1 is required',
-                ],
+                    'required' => 'Status Vaksin 1 is required'
+                ]
             ],
             'status_vaksin_2' => [
                 'rules' => 'required',
                 'errors' => [
-                    'required' => 'Status Vaksin 2 is required',
-                ],
-            ],
-            'desa' => [
-                'rules' => 'required|numeric',
-                'errors' => [
-                    'required' => 'desa is required',
-                ],
-            ],
+                    'required' => 'Status Vaksin 2 is required'
+                ]
+            ]
+            // 'desa' => [
+            //     'rules' => 'required|numeric',
+            //     'errors' => [
+            //         'required' => 'desa is required'
+            //     ]
+            // ]
         ]);
 
-        if ($validation->run() == false) {
+        if ($validation->run() == FALSE) {
             $errors = $validation->getErrors();
             echo json_encode(['code' => 0, 'error' => $errors]);
         } else {
             $id = $this->request->getPost("edit_id");
             $data = [
-                'nama_karyawan' => $this->request->getPost('nama_karyawan'),
-                'usia' => $this->request->getPost('usia'),
-                'status_vaksin_1' => $this->request->getPost('status_vaksin_1'),
-                'status_vaksin_2' => $this->request->getPost('status_vaksin_2'),
+                'nama_karyawan'     => $this->request->getPost('nama_karyawan'),
+                'usia'              => $this->request->getPost('usia'),
+                'status_vaksin_1'   => $this->request->getPost('status_vaksin_1'),
+                'status_vaksin_2'   => $this->request->getPost('status_vaksin_2'),
                 // 'prov'              => $this->request->getPost('prov'),
                 // 'kota'              => $this->request->getPost('kota'),
                 // 'kec'               => $this->request->getPost('kec'),
-                'desa'              => $this->request->getPost('desa')
+                'alamat'              => $this->request->getPost('alamat')
             ];
             $update = $model->update($id, $data);
 
@@ -348,78 +360,6 @@ class Employee extends Controller
         }
     }
 
-    //----------------------- Import csv -----------------------//
-    public function importCsvToDb()
-    {
-        $input = $this->validate([
-            'file' => 'uploaded[file]|max_size[file,8024]|ext_in[file,csv],',
-        ]);
-        if (!$input) {
-            $data['validation'] = $this->validator;
-            return view('index', $data);
-        } else {
-            if ($file = $this->request->getFile('file')) {
-                if ($file->isValid() && !$file->hasMoved()) {
-                    $newName = $file->getRandomName();
-                    $file->move('../public/csvfile', $newName);
-                    $file = fopen("../public/csvfile/" . $newName, "r");
-                    $i = 0;
-                    $numberOfFields = 6;
-                    $csvArr = array();
-
-                    while (($filedata = fgetcsv($file, 1000, ",")) !== false) {
-                        $num = count($filedata);
-                        if ($i > 0 && $num == $numberOfFields) {
-
-                            // $csvArr[$i]['No.'] = $filedata[0];
-                            // $csvArr[$i]['Nama Kayawan'] = $filedata[1];
-                            // $csvArr[$i]['Usia'] = $filedata[2];
-                            // $csvArr[$i]['Status Vaksin 1'] = $filedata[3];
-                            // $csvArr[$i]['Status Vaksin 2'] = $filedata[4];
-                            // $csvArr[$i]['Aksi'] = $filedata[5];
-
-                            // $csvArr[$i]['id' || 'No'] = $filedata[0];
-                            // $csvArr[$i]['nama_karyawan' || 'Nama Karyawan'] = $filedata[1];
-                            // $csvArr[$i]['usia' || 'Usia'] = $filedata[2];
-                            // $csvArr[$i]['status_vaksin_1' || 'Status Vaksin 1'] = $filedata[3];
-                            // $csvArr[$i]['status_vaksin_2' || 'Status Vaksin 2'] = $filedata[4];
-                            // $csvArr[$i]['aksi' || 'Aksi'] = $filedata[5];
-
-                            $csvArr[$i]['id'] = $filedata[0];
-                            $csvArr[$i]['nama_karyawan'] = $filedata[1];
-                            $csvArr[$i]['usia'] = $filedata[2];
-                            $csvArr[$i]['status_vaksin_1'] = $filedata[3];
-                            $csvArr[$i]['status_vaksin_2'] = $filedata[4];
-                            $csvArr[$i]['aksi'] = $filedata[5];
-                            $csvArr[$i]['user_id'] = $filedata[6];
-                        }
-                        $i++;
-                    }
-                    fclose($file);
-                    $count = 0;
-                    foreach ($csvArr as $userdata) {
-                        $employee = new Employee_model();
-                        $findRecord = $employee->where('id', $userdata['id'])->countAllResults();
-                        if ($findRecord == 0) {
-                            if ($employee->insert($userdata)) {
-                                $count++;
-                            }
-                        }
-                    }
-                    session()->setFlashdata('message', $count . ' rows successfully added.');
-                    session()->setFlashdata('alert-class', 'alert-success');
-                } else {
-                    session()->setFlashdata('message', 'CSV file coud not be imported.');
-                    session()->setFlashdata('alert-class', 'alert-danger');
-                }
-            } else {
-                session()->setFlashdata('message', 'CSV file coud not be imported.');
-                session()->setFlashdata('alert-class', 'alert-danger');
-            }
-        }
-        return redirect()->route('/');
-    }
-
     public function uploadEmployee()
     {
         $session = session();
@@ -427,12 +367,14 @@ class Employee extends Controller
         $user_id = $_SESSION['user_id'];
 
         if ($this->request->getMethod() == "post") {
+
             $file = $this->request->getFile("file");
             $file_name = $file->getTempName();
             $employee = array();
             $csv_data = array_map('str_getcsv', file($file_name));
 
             if (count($csv_data) > 0) {
+
                 $index = 0;
 
                 foreach ($csv_data as $data) {
@@ -442,7 +384,7 @@ class Employee extends Controller
                             "usia" => $data[2],
                             "status_vaksin_1" => $data[3],
                             "status_vaksin_2" => $data[4],
-                            "desa" => $data[6],
+                            "alamat" => $data[9],
                             "user_id" => $user_id,
                         );
                     }
@@ -451,11 +393,10 @@ class Employee extends Controller
 
                 $builder = $this->db->table('employees');
                 $builder->insertBatch($employee);
-                $session = session();
-                $session->setFlashdata("success", "Data saved successfully");
+                // $session = session();
+                // $session->setFlashdata("success", "Data saved successfully");
                 return redirect()->to(base_url('/'));
             }
         }
-        return redirect()->route('/');
-    }
+    return redirect()->route('/');    }
 }
