3b4a24fa1af8e8193c733b09fe67eb9dc55025ea
zuhairajamt <zuhairajamt@gmail.com>
2022-08-04 14:55:39 +0700

    yes

--- app/Controllers/Employee.php
+++ app/Controllers/Employee.php
@@ -331,7 +331,7 @@ class Employee extends Controller
                 'usia'         => $this->request->getPost('usia'),
                 'status_vaksin_1'  => $this->request->getPost('status_vaksin_1'),
                 'status_vaksin_2'  => $this->request->getPost('status_vaksin_2'),
-                'alamat'              => $this->request->getPost('desa')
+                'alamat'              => $this->request->getPost('alamat')
             ];
             $update = $model->update($id, $data);
 
